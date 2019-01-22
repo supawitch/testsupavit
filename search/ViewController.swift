d41f27363c2ad6cf4192df12cedcd9415d6820ac
Admin <admin@MAC26.local>
2019-01-22 11:24:29 +0700

    first commit

--- search/ViewController.swift
+++ search/ViewController.swift
@@ -10,11 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var search: UISearchBar!
+    @IBOutlet weak var table: UITableView!
+    
+    let color = ["red","yellow","green","black","white","blue"]
+    
+    var color1 = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
 }
+    extension ViewController:UITableViewDataSource,UITableViewDelegate{
+        func tableView(_ tableView: UITableView,numberOfRowsInSection section:Int)->Int{
+            return color.count
+        }
+        func tableView(_ tableView: UITableView,cellForRowAt indexPath: IndexPath)-> UITableViewCell{
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+            cell?.textLabel?.text = color[indexPath.row]
+            return cell!
+        }
+    }
+
+
+
+
+
 
